1369262d7fc62ee6c01d770c857ebd741945d395
sparklingwtr1 <KelvinRoel123@gmail.com>
2024-11-28 09:20:02 +0800

    update

--- src/main/menu.js
+++ src/main/menu.js
@@ -110,7 +110,7 @@ function Menu() {
       </div>
 
       <button
-        className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white px-5 py-3 rounded-full shadow-lg transition duration-300"
+        className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white px-5 py-3 rounded-full shadow-lg transition duration-300 z-50"
         onClick={toggleSidebar}
       >
         View Orders
